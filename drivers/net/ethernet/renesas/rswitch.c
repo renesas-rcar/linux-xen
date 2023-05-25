02bb4d0e24214574b617581b10921b43cd0b56a7
Dat Vo <dat.vo.yb@renesas.com>
2023-05-25 14:38:48 +0700

    net: ethernet: renesas: rswitch: Add Xen PV back-end driver
    Add implementation for VMQ Xen PV back-end driver. Front-end will be
    added in the next patch.
    
    Signed-off-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
    Signed-off-by: Dat Vo <dat.vo.yb@renesas.com>

--- drivers/net/ethernet/renesas/rswitch.c
+++ drivers/net/ethernet/renesas/rswitch.c
@@ -971,6 +971,19 @@ static void rswitch_enadis_data_irq(struct rswitch_private *priv, int index, boo
 	rs_write32(BIT(index % 32) | tmp, priv->addr + offs);
 }
 
+void rswitch_enadis_rdev_irqs(struct rswitch_device *rdev, bool enable)
+{
+	if (!rswitch_is_front_dev(rdev)) {
+		rswitch_enadis_data_irq(rdev->priv, rdev->rx_chain->index,
+					enable);
+		rswitch_enadis_data_irq(rdev->priv, rdev->tx_chain->index,
+					enable);
+	} else {
+		if (enable)
+			rswitch_vmq_front_rx_done(rdev);
+	}
+}
+
 static void rswitch_ack_data_irq(struct rswitch_private *priv, int index)
 {
 	u32 offs = GWDIS0 + (index / 32) * 0x10;
@@ -1169,7 +1182,6 @@ int rswitch_poll(struct napi_struct *napi, int budget)
 {
 	struct net_device *ndev = napi->dev;
 	struct rswitch_device *rdev = netdev_priv(ndev);
-	struct rswitch_private *priv = rdev->priv;
 	unsigned long flags;
 	int quota = budget;
 
@@ -1184,11 +1196,10 @@ retry:
 	netif_wake_subqueue(ndev, 0);
 
 	if (napi_complete_done(napi, budget - quota)) {
-		spin_lock_irqsave(&priv->lock, flags);
+		spin_lock_irqsave(&rdev->priv->lock, flags);
 		/* Re-enable RX/TX interrupts */
-		rswitch_enadis_data_irq(priv, rdev->tx_chain->index, true);
-		rswitch_enadis_data_irq(priv, rdev->rx_chain->index, true);
-		spin_unlock_irqrestore(&priv->lock, flags);
+		rswitch_enadis_rdev_irqs(rdev, true);
+		spin_unlock_irqrestore(&rdev->priv->lock, flags);
 	}
 
 	__iowmb();
@@ -2016,8 +2027,7 @@ static int rswitch_open(struct net_device *ndev)
 	pr_debug("%s: tx = %d, rx = %d\n", __func__, rdev->tx_chain->index, rdev->rx_chain->index);
 
 	spin_lock_irqsave(&rdev->priv->lock, flags);
-	rswitch_enadis_data_irq(rdev->priv, rdev->tx_chain->index, true);
-	rswitch_enadis_data_irq(rdev->priv, rdev->rx_chain->index, true);
+	rswitch_enadis_rdev_irqs(rdev, true);
 	spin_unlock_irqrestore(&rdev->priv->lock, flags);
 
 	iowrite32(GWCA_TS_IRQ_BIT, rdev->priv->addr + GWTSDIE);
@@ -2964,7 +2974,10 @@ static irqreturn_t __maybe_unused rswitch_data_irq(struct rswitch_private *priv,
 			continue;
 
 		rswitch_ack_data_irq(priv, c->index);
-		rswitch_queue_interrupt(c->ndev);
+		if (!c->back_info)
+			rswitch_queue_interrupt(c->ndev);
+		else
+			rswitch_vmq_back_data_irq(c);
 	}
 
 	return IRQ_HANDLED;
