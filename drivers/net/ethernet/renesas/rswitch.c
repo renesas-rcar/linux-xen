xen-troops/linux

rswitch: vmq: complete switch do GWCA1
Renesas changed default GWCA to GWCA1 from GWCA0. Some portions of the
VMQ-related code was not ready for this, which caused non-working vmq0
interface.

Rework start_xmit to use CSD1, remove hardcoded magic values,
change device port gwca.index in _front and _back drivers, set
gwca.index to 4 in _front driver.

Signed-off-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

drivers/net/ethernet/renesas/rswitch.c
@@ -1879,7 +1879,8 @@ static int rswitch_start_xmit(struct sk_buff *skb, struct net_device *ndev)
 		desc->info1 = (rdev->ts_tag << 8) | BIT(3);
 	}
 
-	desc->info1 |= ((u64)rdev->remote_chain << 32) | ((BIT(rdev->port)) << 48) |  BIT(2);
+	desc->info1 |= ((u64)rdev->remote_chain << DESC_INFO1_CSD1_SHIFT) |
+		((BIT(rdev->port)) << DESC_INFO1_DV_SHIFT) |  DESC_INFO1_FMT;
 
 	skb_tx_timestamp(skb);
 	dma_wmb();

renesas-rcar/linux-xen

drivers/net/ethernet/renesas/rswitch.c
@@ -2173,7 +2173,8 @@ static int rswitch_start_xmit(struct sk_buff *skb, struct net_device *ndev)
 		skb_tx_timestamp(skb);
 	}
 
-	desc->info1 |= ((u64)rdev->remote_chain << 32) | ((BIT(rdev->port)) << 48) |  BIT(2);
+	desc->info1 |= ((u64)rdev->remote_chain << INFO1_CSD1_SHIFT) |
+		((BIT(rdev->port)) << INFO1_DV_SHIFT) | INFO1_FMT;
 
 	dma_wmb();
 